f98009bd9c5c3517229a5a8aad5841c52ccbc435
shanky <64212617+shanky930j@users.noreply.github.com>
2024-10-27 13:36:07 +0530

    ADMIN product list

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-import express from 'express'
-import colors from 'colors'
-import dotenv from 'dotenv'
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
-import cors from 'cors'
+import cors from 'cors';
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
-const app = express()
+const app = express();
 
 connectDB();
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use('/api/v1/auth', authRoutes)
 
-
+// Route middlewares
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/product', productRoutes); // Add product routes here
 
 app.get('/', (req, res) => {
-    res.send("<h1>Welcome to Resilfy</h1>"
-    )
-})
+    res.send("<h1>Welcome to Resellify</h1>");
+});
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server Running on ${PORT} `.bgCyan.white);
-})
+});
